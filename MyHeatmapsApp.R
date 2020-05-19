c3565348a80a0ee354b082c728fee201d8dc31f9
Dane Limjoco <danelimjoco@Danes-MacBook-Pro.local>
2020-05-19 18:57:44 -0400

    added metadata

--- MyHeatmapsApp.R
+++ MyHeatmapsApp.R
@@ -8,18 +8,60 @@ ui <- fluidPage(titlePanel(h1("RNA-Seq Shiny App!",align = "center")), theme = s
                 
                            fluidRow(
                              column(4, align="center", offset=4,
-                                    h3(fileInput("countsFile", "Choose xlsx File:",
+                                    h3(fileInput("countsFile", "1. Choose count data file (xlsx):",
                                               multiple = TRUE,
                                               accept = c("text/csv",
                                                          "text/comma-separated-values,text/plain",
                                                          ".csv",
                                                          ".xlsx")), 
                                        )
-                             )#,
-                             #column(12, h3("Preview of uploaded data", align="center")
-                             #)
+                             )
                            ),
-                           conditionalPanel(condition = "output.fileUploaded", h3("Preview of uploaded data"), tableOutput('contents'), align="center")
+                           conditionalPanel(condition = "output.fileUploaded", 
+                                            h3("Preview of uploaded data"), 
+                                            tableOutput('contents'), 
+                                            align="center"
+                           ),
+                           br(), #some space 
+                           conditionalPanel(condition = "output.fileUploaded", 
+                                            h3(fileInput("metadataFile", "2. Choose metadata file (xlsx):",
+                                                         multiple = TRUE,
+                                                         accept = c("text/csv",
+                                                                    "text/comma-separated-values,text/plain",
+                                                                    ".csv",
+                                                                    ".xlsx")),
+                                             ),   
+                                            align="center"
+                           ),
+                           br(),
+                           br(), #some space
+                           conditionalPanel(condition = "output.metadataUploaded", 
+                                            h3("3. Select Filters"), 
+                                            
+                                            # Input: Simple integer interval ----
+                                            sliderInput("integer", "Integer:",
+                                                        min = 0, max = 1000,
+                                                        value = 500),
+                                            
+                                            # Input: Decimal interval with step value ----
+                                            sliderInput("decimal", "Decimal:",
+                                                        min = 0, max = 1,
+                                                        value = 0.5, step = 0.1),
+    
+                                            align="center",
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                                            br(), #some space 
+                           ),
+                          br(), #some space 
+                          br(), #some space 
                   
 ) # fluidPage
 
@@ -30,18 +72,30 @@ ui <- fluidPage(titlePanel(h1("RNA-Seq Shiny App!",align = "center")), theme = s
 
 server <- function(input, output) {
   
+  # Count data
   output$contents <- renderTable({
     inFile <- input$countsFile
     read_excel(inFile$datapath, range = "A1:D6")
   })
   
+  # Metadata
+  output$contents2 <- renderTable({
+    inFile <- input$metadataFile
+    read_excel(inFile$datapath)
+  })
+  
+  
   output$fileUploaded <- reactive({
     return(!is.null(input$countsFile))
   })
+  
+  output$metadataUploaded <- reactive({
+    return(!is.null(input$metadataFile))
+  })
+  
   outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
+  outputOptions(output, 'metadataUploaded', suspendWhenHidden=FALSE)
   
-  
-
 } # server
 
 ######## CREATE APP ########
--- featureCounts.R
+++
@@ -1,8 +0,0 @@
-library(Rsubread)
-
-filepath <- "/Users/danelimjoco/Desktop/GithubRepos/MyHeatmapsWebApp/toy.sam"
-counts <- featureCounts(filepath)
-counts
-
-deseqdata <- DESeqDataSetFromMatrix(countData=counts, colData=sampleInfo, design=~condition)
-
