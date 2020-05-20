623a04996983537814eaac321441b7fb8bea592e
Dane Limjoco <danelimjoco@Danes-MacBook-Pro.local>
2020-05-20 16:09:40 -0400

    able to filter out low read counts now

--- MyHeatmapsApp.R
+++ MyHeatmapsApp.R
@@ -2,11 +2,19 @@ library(shiny)
 library(shinythemes)
 library(readxl)
 
+
+# Can put stuff from other script here
+# Remove genes with less than 10 counts across samples (double-check how to select threshold)
+ 
+ #cervical<- read_excel(inFile$datapath, range = "A1:D6")
+ #cervical <- cervical[rowSums(cervical)>10,]
+
+
+
 ######## UI ########
 
 ui <- fluidPage(titlePanel(h1("RNA-Seq Shiny App!",align = "center")), theme = shinytheme("cerulean"),
-                
-                           fluidRow(
+                 fluidRow(
                              column(4, align="center", offset=4,
                                     h3(fileInput("countsFile", "1. Choose count data file (xlsx):",
                                               multiple = TRUE,
@@ -33,21 +41,24 @@ ui <- fluidPage(titlePanel(h1("RNA-Seq Shiny App!",align = "center")), theme = s
                                              ),   
                                             align="center"
                            ),
-                           br(),
+                           br(), #some space
                            br(), #some space
                            conditionalPanel(condition = "output.metadataUploaded", 
-                                            h3("3. Select Filters"), 
+                                            h3("3. Filter out low counts"), 
                                             
                                             # Input: Simple integer interval ----
                                             sliderInput("lowCount", textOutput("lowCounts"),
                                                         min = 0, max = 1000,
                                                         value = 100),
                                             
-                                            # Input: Decimal interval with step value ----
-                                            sliderInput("decimal", "Decimal:",
-                                                        min = 0, max = 1,
-                                                        value = 0.5, step = 0.1),
-    
+                                            actionButton("sendLowCount", "Done!", style="color: #	#87CEFA; background-color: #00FF7F; border-color: #2e6da4"),
+                                            
+                                            br(), #some space 
+                                            br(), #some space 
+                                            
+                                            #h4(textOutput("lowCountMsg")),
+                                            h4(textOutput("genesLeft")),
+                                            
                                             align="center",
                                             br(), #some space 
                                             br(), #some space 
@@ -65,9 +76,6 @@ ui <- fluidPage(titlePanel(h1("RNA-Seq Shiny App!",align = "center")), theme = s
                   
 ) # fluidPage
 
-
-
-
 ######## SERVER ########
 
 server <- function(input, output) {
@@ -83,11 +91,7 @@ server <- function(input, output) {
     inFile <- input$metadataFile
     read_excel(inFile$datapath)
   })
-  
-  output$lowCounts <- renderText({
-    paste0("Eliminate genes with less than ", input$lowCount, " read counts across all samples")
-  })
-  
+
   output$fileUploaded <- reactive({
     return(!is.null(input$countsFile))
   })
@@ -96,6 +100,29 @@ server <- function(input, output) {
     return(!is.null(input$metadataFile))
   })
   
+  # Low counts filter header
+  output$lowCounts <- renderText({
+    paste0("Drop genes with less than ", input$lowCount, " read counts across all samples")
+  })
+  
+  # Low counts filter confirmation message
+  #output$lowCountMsg <- renderText({
+  #  if (input$sendLowCount>0) { 
+  #    return(paste0("Genes with less than ", input$lowCount, " reads across samples dropped!"))
+  #  }
+  #})
+  
+  # Genes remaining message
+  output$genesLeft <- renderText({
+    if (input$sendLowCount>0) { 
+      file <- input$countsFile
+      req(file)
+      countData <- read_excel(file$datapath)
+      reducedCountData <- countData[rowSums(countData[,-1])>input$lowCount,]
+      return(paste0(nrow(countData)-nrow(reducedCountData)," genes dropped! ", nrow(reducedCountData), " genes left for analysis."))
+    }
+  })
+
   outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
   outputOptions(output, 'metadataUploaded', suspendWhenHidden=FALSE)
   
